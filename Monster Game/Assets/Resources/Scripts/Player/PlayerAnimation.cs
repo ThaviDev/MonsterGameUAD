a29dd7125834fb9745b9d7502ca56e79121562d5
ElDeividDev <38382030+ThaviDev@users.noreply.github.com>
2025-11-13 17:16:25 -0600

    Imports for test purpuses

--- Monster Game/Assets/Resources/Scripts/Player/PlayerAnimation.cs
+++ Monster Game/Assets/Resources/Scripts/Player/PlayerAnimation.cs
@@ -1,16 +1,30 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Animator _animator;
+    int _movementStatus;
+    [SerializeField] PlayerMovement _Pmovement;
+
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        _movementStatus = _Pmovement.GetMovementStatus;
+        switch (_movementStatus)
+        {
+            case 0:
+                _animator.SetBool("IsIdle",true);
+                break;
+            case 1:
+                _animator.SetBool("IsIdle",false);
+                break;
+            default:
+                print("Not valid status for anim");
+                break;
+        }
     }
 }
---
+++ Monster Game/Assets/Resources/Scripts/Testing/C_TestOut_Player.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class C_TestOut_Player : MonoBehaviour
+{
+    float _life;
+    float _fear;
+    float _stamina;
+    float _BPM_Total;
+    float _curSpeed;
+    float _curBatery;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    void UseItemInventory()
+    {
+
+    }
+    void InteractWithObjScene()
+    {
+
+    }
+    float IncreaseSpeed()
+    {
+
+        return 0;
+    }
+    bool ToggleFlashlight()
+    {
+
+        return false;
+    }
+    void DashJump()
+    {
+
+    }
+}
