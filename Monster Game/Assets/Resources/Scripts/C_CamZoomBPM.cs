fbcfcc9efce2313e63ac0e45bbb970841c33546a
ElDeividDev <38382030+ThaviDev@users.noreply.github.com>
2026-01-02 17:52:39 -0600

    Camera Zoom con BPM

--- Monster Game/Assets/Resources/Scripts/C_CamZoomBPM.cs
+++ Monster Game/Assets/Resources/Scripts/C_CamZoomBPM.cs
@@ -1,12 +1,33 @@
 using UnityEngine;
-
 public class C_CamZoomBPM : MonoBehaviour
 {
     [SerializeField] Camera m_Cam;
     [SerializeField] C_PlayerStats m_Heart;
+    [SerializeField] float m_SmoothSpeed;
+
+    private float m_TargetSize;
+    private float m_CurrentSize;
+    private float m_velocity; // Variable interna para el SmoothDamp
+    private void Start()
+    {
+        if (m_Cam != null)
+        {
+            m_CurrentSize = m_Cam.orthographicSize;
+        }
+    }
     void Update()
     {
         if (m_Heart != null)
-        m_Cam.orthographicSize = 5f - (m_Heart.GetBPM - 80f) * 0.025f;
+        {
+            //m_Cam.orthographicSize = 5f - (m_Heart.GetBPM - 80f) * 0.025f;
+            m_TargetSize = 5f - (m_Heart.GetBPM - 80f) * 0.025f;
+
+            // SmoothDamp modifica 'm_velocity' internamente
+            // Para el próximo frame, 'm_velocity' ya tiene el valor actualizado
+            m_CurrentSize = Mathf.SmoothDamp(m_CurrentSize, m_TargetSize, ref m_velocity, m_SmoothSpeed);
+
+            // Aplicar nuevo tamanio
+            m_Cam.orthographicSize = m_CurrentSize;
+        }
     }
 }
